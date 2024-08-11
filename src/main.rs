2411fe083eb36459c22296eb366fe8be32d1fb59
pulkitgangwar <pulkitgangwar2016@gmail.com>
2024-08-11 12:45:40 +0530

    added logic for media

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod content;
+
+use content::catalog::Catalog;
+use content::media::Media;
+
 fn main() {
-    println!("Hello, world!");
+    let book = Media::Book {
+        title: String::from("Atomic Habits"),
+        author: String::from("Bhakul das"),
+    };
+    let mut catalog = Catalog::new();
+
+    catalog.add(book);
+    match catalog.get_by_index(100) {
+        Option::Some(value) => {
+            println!("{:#?}", value);
+        }
+        Option::None => {
+            println!("No value found");
+        }
+    }
+
+    println!("{:#?}", catalog);
 }
